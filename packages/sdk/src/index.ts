a5391291a9be9b07bd820678bde692e8167acc65
8harath <8harath.k@gmail.com>
2026-05-15 09:44:32 +0530

    chore(sdk): export loadProfile + types from index

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,1 +1,4 @@
 export { SCHEMA_VERSION } from '@wv/spec';
+export type { Profile } from '@wv/spec';
+export { loadProfile, loadProfileFromString } from './loadProfile.js';
+export { ProfileValidationError } from './errors.js';

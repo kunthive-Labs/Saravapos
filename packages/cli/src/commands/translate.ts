a93f339c98930fa815926db4dd63ce02e159fd9f
8harath <8harath.k@gmail.com>
2026-05-23 06:37:00 +0530

    feat(cli): translate --input <path> flag (file)

--- packages/cli/src/commands/translate.ts
+++ packages/cli/src/commands/translate.ts
@@ -4,6 +4,7 @@ export const translateCommand = new Command('translate')
   .description('Translate text between two worldview profiles')
   .requiredOption('--from <path>', 'path to source profile YAML')
   .requiredOption('--to <path>', 'path to destination profile YAML')
+  .option('--input <path>', 'read source text from file')
   .action(() => {
     console.log('translate: not yet implemented');
   });

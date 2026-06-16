8ef99c8ace8cb6eac4f7189c438d911f14c46175
8harath <8harath.k@gmail.com>
2026-06-16 19:28:13 +0530

    feat(sdk): add analogy module barrel export

---
+++ packages/sdk/src/analogy/index.ts
@@ -0,0 +1,8 @@
+export { tokenize, tokenizeRuns } from './tokenize.js';
+export { extractConcepts, type ExtractOptions } from './extractConcepts.js';
+export {
+  matchAnalogies,
+  scoreEntry,
+  type AnalogyMatch,
+  type MatchOptions,
+} from './matchAnalogies.js';
